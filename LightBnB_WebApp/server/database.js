04cada24f5a417f7e76e0afed892ff41739d4b44
shivanix <shvni.here@gmail.com>
2021-07-23 01:00:49 +0000

    updated addProperty function to enable adding new property to the properties table

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,4 +1,4 @@
-const properties = require('./json/properties.json');
+//const properties = require('./json/properties.json');
 // const users = require('./json/users.json');
 
 const {
@@ -185,21 +185,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // return pool
-  // .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES`, [property])
-  //   .then((result) => {
-  //     // console.log(result.rows);
-  //     return result.rows;
+  return pool
+    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
+    
+      [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
 
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //   });
+    .then((result) => {
+      //console.log(result.rows);
+      return result.rows;
 
-
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.addProperty = addProperty;
